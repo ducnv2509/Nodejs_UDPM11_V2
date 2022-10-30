3be50c8566a1bbe2f3784313092d381ed3619a8c
ducnv2509 <ducit2509@gmail.com>
2022-10-31 01:26:20 +0700

    update API

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,11 +1,19 @@
 import { OK, SYSTEM_ERROR } from "../constant/HttpResponseCode.js";
 import query from "../helper/helperDb.js";
+import myLogger from "../winstonLog/winston.js";
 
 
 export async function getAllProduct() {
     let sql = `call getAllProduct()`;
+    let products = [];
     let ret = { statusCode: SYSTEM_ERROR, error: 'ERROR', description: 'First error!' };
-    const result = await query(sql);
-    ret = { statusCode: OK, data: result[0][0] };
+    let result = await query(sql);
+    result[0].forEach(e => {
+        let { id, image, name, wholesale_price } = e
+        products.push({ id, image, name, wholesale_price })
+    })
+    myLogger.info("%o", result)
+    // products.push({id, image, name, wholesale_price})
+    ret = { statusCode: OK, data: products };
     return ret;
 }
