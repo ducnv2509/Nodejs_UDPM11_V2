f1ea7be48c8ee893256ec76dc2924e45ffa03fb8
ducnv2509 <ducit2509@gmail.com>
2022-11-02 23:56:26 +0700

    fix quantity

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,14 +1,24 @@
-import { OK, SYSTEM_ERROR } from "../constant/HttpResponseCode.js";
+import { BAD_REQUEST, OK, SYSTEM_ERROR } from "../constant/HttpResponseCode.js";
 
 import query from "../helper/helperDb.js";
 import myLogger from "../winstonLog/winston.js";
 
 export async function addToCart(id_user, id_product_varient, quantity) {
     let params = [id_user, id_product_varient, quantity];
+    let paramsConvert = [id_user, id_product_varient];
     let sql = `call createCart(?, ?, ?)`;
+    let convertTo0 = `call convertTo0(?,?)`
+    let sqlQuantitt = `select  ip.quantity from inventories_product_variant ip where ip.product_variant_id =${id_product_varient}`;
     let ret = { statusCode: SYSTEM_ERROR, error: 'ERROR', description: 'First error!' };
-    let result = await query(sql, params);
-    myLogger.info('result %o', result);
-    ret = { statusCode: OK, data: result[0][0] };
+    let resultConvert = await query(convertTo0, paramsConvert);
+    let resultQuantity = await query(sqlQuantitt);
+    myLogger.info('resultQuantity %o', resultQuantity);
+
+    if (resultConvert[0][0].quantity + quantity > resultQuantity[0].quantity) {
+        ret = { statusCode: BAD_REQUEST, error: 'ERROR', description: 'Số lượng trong kho không đủ' };
+    } else {
+        let result = await query(sql, params);
+        ret = { statusCode: OK, data: result[0][0] };
+    }
     return ret;
 }
