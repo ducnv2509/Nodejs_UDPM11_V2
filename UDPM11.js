41a7c2d3b73b106a6b712f3f4162483566af0bb9
ducnv2509 <ducit2509@gmail.com>
2022-11-29 10:14:08 +0700

    done

--- UDPM11.js
+++ UDPM11.js
@@ -44,7 +44,8 @@ app.use((data, req, res, next) => {
     }
 });
 const port = process.env.UDPM11_API_PORT || 3001
-const host = '0.0.0.0'
+const host = '0.0.0.0';
+
 function myListener() {
     myLogger.info(`Listening on port ${port}..`);
 }
